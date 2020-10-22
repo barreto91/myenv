aadaa2936d449e4463f52aeb6fa2944568d76700
Abel Barreto <50952813+barreto91@users.noreply.github.com>
2020-10-22 23:41:52 +0200

    Update lecture_2.py

--- python_gwdg/lecture_2.py
+++ python_gwdg/lecture_2.py
@@ -1,3 +1,6 @@
+import numpy as np
+print("module loaded")
+
 # this is a list
 data_types = ['integer', 'float', 'string', 'list']
 
@@ -12,3 +15,26 @@ print(first_type)
 
 #
 print(data_types[3])### 4 is out of the range
+
+
+###
+
+# length of the list data_types
+number_of_types = len(data_types)
+
+print("There are {} elements in the list data_types." \
+      .format(number_of_types))
+#
+# create a list
+animals = ['cat', 'dog', 'bird', 'horse']
+print(animals)
+
+# assign a new value to the element at index 2 of the list
+animals[2] = 'penguin'
+print(animals)
+
+
+###loop for a list
+
+for typ in data_types:
+    print(typ)
