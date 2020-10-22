c27f3318d8bb16b9e54868a50405c6ca9e0f2122
Abel Barreto <50952813+barreto91@users.noreply.github.com>
2020-10-22 23:11:06 +0200

    Create lecture_2.py

---
+++ python_gwdg/lecture_2.py
@@ -0,0 +1,14 @@
+# this is a list
+data_types = ['integer', 'float', 'string', 'list']
+
+# and like this you can iterate its items
+for typ in data_types:
+    print ("{} is a data type in Python.".format(typ))
+
+#
+# access the element in position 0
+first_type = data_types[0]
+print(first_type)
+
+#
+print(data_types[3])### 4 is out of the range
